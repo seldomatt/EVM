c2a0f334342e322a44c1e3708dc3e878079971e0
matthewsalerno <matthewsalern@gmail.com>
2012-09-28 16:34:47 -0400

    added a few methods

--- evmnew.rb
+++ evmnew.rb
@@ -3,7 +3,7 @@ require './orderclass'
 
 extra_value_menu = {:meals => {1 =>{:name => 'Big Mac', :price => 5.00},
 								2 =>{:name => 'Quarter Pounder', :price => 5.25}}, 
-					:drinks => ["Coke", "Sprite", "Dr.Pepper"]}
+					:drinks => ["coke", "sprite", "dr.pepper"]}
 
 
 
--- orderclass.rb
+++ orderclass.rb
@@ -25,6 +25,26 @@ class Outputter
 		puts "Sorry, didn't understand that command"
 	end
 
+	def self.meal_choice_prompt
+		puts "Which meal would you like? (type '1', '2', etc.)"
+	end
+
+	def self.no_choice_error
+		puts "Sorry, that's not a choice, please try again"
+	end
+
+	def self.meal_choice_confirm(order)
+		puts "A #{order.name} meal, great choice!"
+	end
+
+	def self.drink_choice_prompt(order)
+		puts "What kind of drink would you like with your #{order.name}"
+	end
+
+	def self.drink_choice_confirm(order)
+		puts "Ok, so that's a #{order.name} meal with a #{order.drink}"
+	end
+
 end
 
 
@@ -41,15 +61,11 @@ class DriveThru
 
 		custinput = gets.downcase.chomp
 
-		while custinput != "quit"
-
-			if custinput == 'driveup'
-				choose_meal
-			else 
-				Outputter.no_command_error
-			end
+		if custinput == 'driveup'
+			choose_meal
+		else 
+			Outputter.no_command_error
 		end
-	
 	end
 
 	def display_menu
@@ -63,31 +79,55 @@ class DriveThru
 
 		display_menu
 
-		puts "Which meal would you like? (type '1', '2', etc.)"
+		Outputter.meal_choice_prompt 
 
 		mealchoice = gets.chomp.to_i
 
 		if @menu[:meals].has_key?(mealchoice)
 			order = Orders.new(mealchoice, @menu[:meals][mealchoice][:name], @menu[:meals][mealchoice][:price])
-			puts order.inspect
+			Outputter.meal_choice_confirm(order)
 			choose_drink(order)
 		else 
-			puts "That's not a menu choice, try again"
+			Outputter.no_choice_error
 		end
 	
 	end
 
-	def display_drinks(order)
+	def display_drinks
 		@menu[:drinks].each do |drink|
-			puts "#{order.name} with a #{drink}?"
+			puts "a #{drink.capitalize}?"
 		end
 	end
 
 
 	def choose_drink(order)
-		display_drinks(order)
+		Outputter.drink_choice_prompt(order)
+		
+		display_drinks
+
+		drinkchoice = gets.chomp.downcase
+
+		if @menu[:drinks].include?(drinkchoice)
+			order.drink = drinkchoice.capitalize
+			Outputter.drink_choice_confirm(order)
+			supersize(order)
+		else 
+			Outputter.no_choice_error
+		end
 
 	end
+
+	def supersize(order)
+	end 
+
+
+
+
+
+
+
+
+
 
 
 end
